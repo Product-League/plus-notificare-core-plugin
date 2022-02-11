8430bd87a248da20b26c90c4b1aedca74519d0b9
Helder Pinhal <helder@notifica.re>
2022-02-11 16:24:06 +0000

    feat(scannables): add plugin

---
+++ packages/cordova-plugin-notificare-scannables/src/js/index.ts
@@ -0,0 +1,8 @@
+import { NotificareScannables } from './notificare-scannables';
+import { bootstrap } from './events';
+
+export * from './models/notificare-scannable';
+
+export default NotificareScannables;
+
+bootstrap();

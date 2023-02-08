24cb617d5bf15df6ba88f11e221d0b0279fb3010
yevheniismirnov <yevhenii.smirnov@outlook.com>
2023-02-08 16:16:46 +0000

    feat(push): include the delivery mechanism for notification received events

--- packages/cordova-plugin-notificare-push/src/js/index.ts
+++ packages/cordova-plugin-notificare-push/src/js/index.ts
@@ -1,6 +1,7 @@
 import { NotificarePush } from './notificare-push';
 import { bootstrap } from './events';
 
+export * from './models/notificare-notification-delivery-mechanism';
 export * from './models/notificare-system-notification';
 
 export default NotificarePush;
